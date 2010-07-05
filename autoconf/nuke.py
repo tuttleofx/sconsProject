033e4ac113e20113c4ef6633fae5b673169d3015
Fabien Castan <fabien.castan@free.fr>
2010-07-05 13:16:43 +0200

    nuke: add standard includes
    recent gcc doesn't include it by default anymore

--- autoconf/nuke.py
+++ autoconf/nuke.py
@@ -3,7 +3,7 @@ from glew import *
 from pthread import *
 
 nuke = LibWithHeaderChecker( ['DDImage'],
-                             ['iostream','DDImage/NukeWrapper.h','Build/fnBuild.h'],
+                             ['cstdarg','memory','iostream','DDImage/NukeWrapper.h','Build/fnBuild.h'],
                              language='c++',
                              name='nuke',
                              dependencies=[glew, pthread] )
