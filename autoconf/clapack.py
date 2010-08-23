4efe699af21acd57d1d770daa4cf2489e9357ea4
Fabien Castan <fabien.castan@free.fr>
2010-08-23 17:11:19 +0200

    minor

--- autoconf/clapack.py
+++ autoconf/clapack.py
@@ -1,4 +1,11 @@
 from _external import *
+from blas import *
+from f2c import *
 
-clapack = LibWithHeaderChecker(['lapack','blas','f2c'], ['f2c.h','clapack.h'], 'c', name='clapack')
+clapack = LibWithHeaderChecker( ['lapack'],
+                                ['f2c.h','clapack.h'],
+                                'c',
+                                name='clapack',
+                                dependencies=[blas,f2c]
+                               )
 
--- autoconf/nvidiaSDI.py
+++ autoconf/nvidiaSDI.py
@@ -1,3 +1,11 @@
 from _external import *
+from gl import *
+from glu import *
 
-nvidiaSDI = LibWithHeaderChecker(['XNVCtrl', 'anc', 'X11', 'Xxf86vm', 'Xi', 'Xmu', 'GL', 'GLU'],'NVCtrl.h','c',name='nvidiaSDI',defines=['GL_GLEXT_PROTOTYPES', 'GLX_GLXEXT_PROTOTYPES'])
+nvidiaSDI = LibWithHeaderChecker( ['XNVCtrl', 'anc', 'X11', 'Xxf86vm', 'Xi', 'Xmu'],
+                                  ['NVCtrl.h'],
+                                  'c',
+                                  name='nvidiaSDI',
+                                  defines=['GL_GLEXT_PROTOTYPES', 'GLX_GLXEXT_PROTOTYPES'],
+                                  dependencies=[gl, glu],
+                                 )
