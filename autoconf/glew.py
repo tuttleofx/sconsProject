8215c79bd9fed9ad31c8d8791cba35deb0946f00
Alexandre Parent <barbak@gmail.com>
2010-08-24 11:43:52 +0200

    Fixing glew dependencies

--- autoconf/glew.py
+++ autoconf/glew.py
@@ -1,8 +1,20 @@
 from _external import *
+from glu import *
+from gl import *
 
 if windows:
-    glew = LibWithHeaderChecker('glew32','GL/glew.h','c', name='glew')
+    glew = LibWithHeaderChecker('glew32',
+                                'GL/glew.h',
+                                'c',
+                                name='glew',
+                                dependencies=[glu,gl]
+                                )
 else:
-    glew = LibWithHeaderChecker('GLEW','GL/glew.h','c',name='glew')
+    glew = LibWithHeaderChecker('GLEW',
+                                'GL/glew.h',
+				'c',
+				name='glew',
+                                dependencies=[glu,gl]
+				)
 
 
