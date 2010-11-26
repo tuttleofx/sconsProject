5b667342e7de0be0ed597b3e1c70863bb051d1e0
Fabien Castan <fabien.castan@free.fr>
2010-11-26 15:33:31 +0100

    change boost_thread default library name (without "-mt")

--- autoconf/boost_thread.py
+++ autoconf/boost_thread.py
@@ -1,6 +1,6 @@
 from _external import *
 from boost import *
 
-boost_thread = LibWithHeaderChecker( 'boost_thread-mt', 'boost/thread.hpp', 'c++',
+boost_thread = LibWithHeaderChecker( 'boost_thread', 'boost/thread.hpp', 'c++',
                                      name='boost_thread', dependencies=[boost] )
 
