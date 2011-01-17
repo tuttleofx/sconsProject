d8dee05a86d4e4be72b88487754198e0aea6f800
Fabien Castan <fabien.castan@free.fr>
2011-01-17 18:11:20 +0100

    boost_graph: add dependency to boost_regex

--- autoconf/boost_graph.py
+++ autoconf/boost_graph.py
@@ -1,6 +1,8 @@
 from _external import *
 from boost import *
+from boost_regex import *
 
 boost_graph = LibWithHeaderChecker( 'boost_graph', 'boost/graph/adjacency_list.hpp', 'c++',
-                                    dependencies=[boost] )
+                                    dependencies=[boost, boost_regex] )
+
 
