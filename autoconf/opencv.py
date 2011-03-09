f3cef57c78af493e170c77bc64a4d6a5f0f6769c
Eloi du Bois <edubois@quintaindustries.com>
2011-03-09 14:02:50 +0100

    Added gpu for opencv.py

--- autoconf/opencv.py
+++ autoconf/opencv.py
@@ -1,4 +1,4 @@
 from _external import *
 
-opencv = LibWithHeaderChecker(['opencv_core', 'opencv_video', 'opencv_calib3d', 'opencv_highgui', 'opencv_imgproc', 'opencv_ml'],
+opencv = LibWithHeaderChecker(['opencv_core', 'opencv_video', 'opencv_calib3d', 'opencv_highgui', 'opencv_imgproc', 'opencv_ml', 'opencv_gpu', 'opencv_ml', 'opencv_contrib'],
                               'opencv/cv.h', 'c', name='opencv')
