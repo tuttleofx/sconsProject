224ae298d69176de050bac79062f8c731c203146
Eloi du Bois <edubois@quintaindustries.com>
2011-03-10 15:03:28 +0100

    Disabled gpu module.

--- autoconf/opencv.py
+++ autoconf/opencv.py
@@ -1,4 +1,5 @@
 from _external import *
+#opencv = LibWithHeaderChecker(['opencv_core', 'opencv_video', 'opencv_calib3d', 'opencv_highgui', 'opencv_imgproc', 'opencv_ml', 'opencv_gpu', 'opencv_ml', 'opencv_contrib'], 'opencv/cv.h', 'c', name='opencv')
 
-opencv = LibWithHeaderChecker(['opencv_core', 'opencv_video', 'opencv_calib3d', 'opencv_highgui', 'opencv_imgproc', 'opencv_ml', 'opencv_gpu', 'opencv_ml', 'opencv_contrib'],
+opencv = LibWithHeaderChecker(['opencv_core', 'opencv_video', 'opencv_calib3d', 'opencv_highgui', 'opencv_imgproc', 'opencv_ml', 'opencv_ml' ],
                               'opencv/cv.h', 'c', name='opencv')
