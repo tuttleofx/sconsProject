5313cf883a0b218f7933e39e7b823cc8619c6284
Fabien Castan <fcastan@quintaindustries.com>
2011-03-17 11:48:14 +0100

    add ssba define

--- autoconf/ssba.py
+++ autoconf/ssba.py
@@ -5,5 +5,6 @@ ssba = LibWithHeaderChecker( ['V3D','ldl','colamd'],
                              'Geometry/v3d_metricbundle.h',
                              'c++',
                              name='ssba',
+                             defines=['V3DLIB_ENABLE_SUITESPARSE'],
                              dependencies=[glut] )
 
