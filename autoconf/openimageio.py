92b629f6a53904d6aafd4afe3c0cc3f8d1e6d539
Fabien Castan <fabien.castan@free.fr>
2011-11-28 19:10:28 +0100

    oiio: minor

--- autoconf/openimageio.py
+++ autoconf/openimageio.py
@@ -2,7 +2,7 @@ from _external import *
 from dl import *
 
 openimageio = LibWithHeaderChecker(
-            ['OpenImageIO'], 'OpenImageIO/imageio.h', 'c++',
+            ['OpenImageIO'], 'imageio.h', 'c++',
             name='openimageio',
             call='',
             dependencies =[dl] )
