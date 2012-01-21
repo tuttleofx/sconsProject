ab2b83112dd544ffa65c437cdad59a6da5aa080a
Fabien Castan <fabien.castan@free.fr>
2012-01-21 23:34:29 +0100

    gphoto: add gphoto2_port to link

--- autoconf/gphoto2.py
+++ autoconf/gphoto2.py
@@ -1,4 +1,4 @@
 from _external import *
 
-gphoto2 = LibWithHeaderChecker('gphoto2', ['gphoto2/gphoto2.h'], 'c')
+gphoto2 = LibWithHeaderChecker(['gphoto2', 'gphoto2_port'], ['gphoto2/gphoto2.h'], 'c', name='gphoto2')
 
