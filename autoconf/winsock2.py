b72ab35056ca6ec1e48db963d61c31d89ec80161
Du Bois Eloi <eloi.du.bois@gmail.com>
2012-01-31 17:56:09 +0100

    sconsProject: fix on winsock2

--- autoconf/winsock2.py
+++ autoconf/winsock2.py
@@ -1,6 +1,6 @@
 from _external import *
 
-winsock2 = LibWithHeaderChecker( 'winsock2',
+winsock2 = LibWithHeaderChecker( 'ws2_32',
 		                ['winsock2.h'],
 				'c',
-				name='ws2_32' )
+				name='winsock2' )
