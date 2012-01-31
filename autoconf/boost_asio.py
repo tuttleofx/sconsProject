da09d52c4c41b22a04178d22d87a00af49b1877f
Du Bois Eloi <eloi.du.bois@gmail.com>
2012-01-31 18:01:53 +0100

    sconsProject: added windows check in boost_asio.

--- autoconf/boost_asio.py
+++ autoconf/boost_asio.py
@@ -1,6 +1,12 @@
+import os
+import sys
 from _external import *
 from winsock2 import *
 from boost import *
 
-boost_asio = HeaderChecker( 'boost_asio', 'boost/asio.hpp', 'c++', dependencies=[boost, winsock2] )
+windows = os.name.lower() == "nt" and sys.platform.lower().startswith("win")
+if windows:
+	boost_asio = HeaderChecker( 'boost_asio', 'boost/asio.hpp', 'c++', dependencies=[boost, winsock2] )
+else:
+	boost_asio = HeaderChecker( 'boost_asio', 'boost/asio.hpp', 'c++', dependencies=[boost] )
 
