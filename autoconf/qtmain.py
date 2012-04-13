2bb8524b3d2c7e050bcd39fcacbe75cb69ac385f
Du Bois Eloi <eloi.du.bois@gmail.com>
2012-04-13 14:07:17 +0200

    sconsProject: fixed wrong merge.

--- autoconf/qtmain.py
+++ autoconf/qtmain.py
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
 from _external import *
 
 qtmain = LibChecker( 'qtmain' )
-=======
-from _external import *
-
-qtmain = LibChecker( 'qtmain' )
->>>>>>> 1f0103ab5e943b5a5d470c93e5b56ea724ea2b84
