288dd02afdb31f2ee62189d53ea431a4f9daf9b7
Fabien Castan <fabien.castan@free.fr>
2012-09-10 23:05:39 +0200

    declare OpenImageIO dependencies

--- autoconf/openimageio.py
+++ autoconf/openimageio.py
@@ -1,10 +1,11 @@
 from _external import *
+from boost import *
 from dl import *
 
 openimageio = LibWithHeaderChecker(
             ['OpenImageIO'], 'imageio.h', 'c++',
             name='openimageio',
             call='',
-            dependencies =[dl] )
+            dependencies =[boost,dl] )
 
 
