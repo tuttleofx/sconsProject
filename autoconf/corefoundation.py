abe923c31dd4b320983580156563dc56aa3b87f7
alx <barbak@gmail.com>
2012-11-08 21:55:11 +0100

    add CoreFoundation for osx. BUG: CheckLib doesn't work with frameworks.

---
+++ autoconf/corefoundation.py
@@ -0,0 +1,6 @@
+from _external import *
+
+corefoundation = LibWithHeaderChecker('CoreFoundation',
+                                      'CoreFoundation.h',
+                                      'c++',
+                                      name='corefoundation')
