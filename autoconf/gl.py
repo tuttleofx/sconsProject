30d625aeb8bbc5611a8f4da24cda31620b662616
Ben Hagen <c.ben.hagen@gmail.com>
2013-02-20 20:19:49 +0100

    use GL instead of AGL on osx

--- autoconf/gl.py
+++ autoconf/gl.py
@@ -2,8 +2,6 @@ from _external import *
 
 if windows:
     gl = LibWithHeaderChecker('opengl32', ['windows.h','GL/gl.h'], 'c', name='gl')
-elif macos:
-    gl = LibWithHeaderChecker('OpenGL', ['AGL/gl.h'], 'c', name='gl')
 else : # unix
     gl = LibWithHeaderChecker('GL', ['GL/gl.h'], 'c', name='gl')
 
