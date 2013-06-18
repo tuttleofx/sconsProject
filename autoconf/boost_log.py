d5e37bd2776e445be866c0c3d8cbc07a6d15a120
mfe <mfe@mikrosimage.eu>
2013-06-18 12:08:31 +0200

    Remove rt dependency for window in boost_log script

--- autoconf/boost_log.py
+++ autoconf/boost_log.py
@@ -4,17 +4,30 @@ from boost_date_time import *
 from boost_chrono import *
 from boost_thread import *
 from boost_filesystem import *
-from rt import *
+if not windows:
+	from rt import *
 
-boost_log = LibWithHeaderChecker(
-	'boost_log',
-	['boost/log/core.hpp'],
-	'c++',
-	dependencies = [
+if windows:
+	tmpDep =  [
+		boost,
+		boost_date_time,
+		boost_chrono,
+		boost_thread,
+		boost_filesystem,
+		] 
+else:
+	tmpDep =  [
 		boost,
 		boost_date_time,
 		boost_chrono,
 		boost_thread,
 		boost_filesystem,
 		rt,
-		] )
+		] 
+
+boost_log = LibWithHeaderChecker(
+	'boost_log',
+	['boost/log/core.hpp'],
+	'c++',
+	dependencies = tmpDep
+	 )
