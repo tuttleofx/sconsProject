2fbd94fcc0efc1b38bbb3ac19484e7d3f3b95ef8
Ben Hagen <c.ben.hagen@gmail.com>
2013-09-03 22:16:29 +0200

    Remove rt dependency for macos in boost_log script

--- autoconf/boost_log.py
+++ autoconf/boost_log.py
@@ -4,30 +4,38 @@ from boost_date_time import *
 from boost_chrono import *
 from boost_thread import *
 from boost_filesystem import *
-if not windows:
-	from rt import *
+if not windows and not macos:
+        from rt import *
 
 if windows:
-	tmpDep =  [
-		boost,
-		boost_date_time,
-		boost_chrono,
-		boost_thread,
-		boost_filesystem,
-		] 
+        tmpDep =  [
+                boost,
+                boost_date_time,
+                boost_chrono,
+                boost_thread,
+                boost_filesystem,
+                ]
+if macos:
+        tmpDep =  [
+                boost,
+                boost_date_time,
+                boost_chrono,
+                boost_thread,
+                boost_filesystem,
+                ]
 else:
-	tmpDep =  [
-		boost,
-		boost_date_time,
-		boost_chrono,
-		boost_thread,
-		boost_filesystem,
-		rt,
-		] 
+        tmpDep =  [
+                boost,
+                boost_date_time,
+                boost_chrono,
+                boost_thread,
+                boost_filesystem,
+                rt,
+                ]
 
 boost_log = LibWithHeaderChecker(
-	'boost_log',
-	['boost/log/core.hpp'],
-	'c++',
-	dependencies = tmpDep
-	 )
+        'boost_log',
+        ['boost/log/core.hpp'],
+        'c++',
+        dependencies = tmpDep
+         )
