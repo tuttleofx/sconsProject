1bdbff9aff8c3c4bf289c66c0b943b3d925cfa61
Fabien Castan <fabien.castan@free.fr>
2013-09-04 01:24:45 +0200

    boost_log: minor, code simplification

--- autoconf/boost_log.py
+++ autoconf/boost_log.py
@@ -4,38 +4,25 @@ from boost_date_time import *
 from boost_chrono import *
 from boost_thread import *
 from boost_filesystem import *
-if not windows and not macos:
-        from rt import *
 
-if windows:
-        tmpDep =  [
-                boost,
-                boost_date_time,
-                boost_chrono,
-                boost_thread,
-                boost_filesystem,
-                ]
-if macos:
-        tmpDep =  [
-                boost,
-                boost_date_time,
-                boost_chrono,
-                boost_thread,
-                boost_filesystem,
-                ]
-else:
-        tmpDep =  [
-                boost,
-                boost_date_time,
-                boost_chrono,
-                boost_thread,
-                boost_filesystem,
-                rt,
-                ]
+
+boost_log_dep =  [
+        boost,
+        boost_date_time,
+        boost_chrono,
+        boost_thread,
+        boost_filesystem,
+    ]
+
+if linux:
+    from rt import *
+    boost_log_dep.append(rt)
+
 
 boost_log = LibWithHeaderChecker(
         'boost_log',
         ['boost/log/core.hpp'],
         'c++',
-        dependencies = tmpDep
-         )
+        dependencies = boost_log_dep
+    )
+
