01a54b4e001536098ab53f018117d388df728ed1
mfe <mfe@mikrosimage.eu>
2013-10-09 12:52:13 +0200

    Fix libav dependencies

--- autoconf/avformat.py
+++ autoconf/avformat.py
@@ -1,7 +1,10 @@
 from _external import *
+from avcodec import *
+from avutil import *
 
 avformat = LibWithHeaderChecker(
 				'avformat',
 				'libavformat/avformat.h',
 				'c',
+				dependencies=[avcodec, avutil]
 			)
