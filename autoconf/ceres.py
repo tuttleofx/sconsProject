de0f605e38781fc03f8f3b71a9bad989bb810524
Fabien Castan <fabien.castan@free.fr>
2014-09-20 11:39:43 +0200

    ceres: declare eigen dependency

--- autoconf/ceres.py
+++ autoconf/ceres.py
@@ -1,4 +1,5 @@
 from _external import *
+from eigen import *
 from pthread import *
 from amd import *
 from gomp import *
@@ -6,4 +7,16 @@ from lapack import *
 from suitesparse import *
 from glog import *
 
-ceres = LibWithHeaderChecker('ceres', 'ceres/ceres.h', 'c++', name='ceres', dependencies = [gomp,lapack,suitesparse,amd,pthread,glog],)
+ceres = LibWithHeaderChecker('ceres',
+                             'ceres/ceres.h',
+                             'c++',
+                             name='ceres',
+                             dependencies=[
+                                 eigen,
+                                 gomp,
+                                 lapack,
+                                 suitesparse,
+                                 amd,
+                                 pthread,
+                                 glog],
+                             )
